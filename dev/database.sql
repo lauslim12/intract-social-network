ad1d6950a891ab8b7d1b7e99d71c0f103896b661
Nicholas <nicholasdwiarto@yahoo.com>
2020-02-28 22:28:43 +0700

    Added dev folder for development needs.

---
+++ dev/database.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS intract;
+CREATE DATABASE IF NOT EXISTS intract;
+USE intract;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(25),
+  last_name VARCHAR(25),
+  username VARCHAR(100),
+  email VARCHAR(100),
+  password VARCHAR(255),
+  birthday DATE,
+  gender VARCHAR(2),
+  signup_date DATE,
+  profile_pic VARCHAR(255),
+  num_posts INT,
+  num_likes INT,
+  user_closed VARCHAR(3),
+  friend_array TEXT,
+  PRIMARY KEY (id)
+);
