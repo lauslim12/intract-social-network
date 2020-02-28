87c7e282508045cc64c0fe4af00e1e389213ce23
Nicholas <nicholasdwiarto@yahoo.com>
2020-02-28 10:33:04 +0700

    Added baseline for registration.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Welcome to Intract!</title>
+  </head>
+
+  <body>
+    <form action='' method="POST">
+      <input type="text" name="fname" placeholder="First Name" />
+      <input type="text" name="lname" placeholder="Last Name" />
+      <input type="text" name="username" placeholder="username" />
+      <input type="email" name="email" placeholder="email" />
+      <input type="password" name="password" placeholder="password" />
+      <input type="date" name="birthdate" />
+      <input type="radio" name="gender" value="M">Male
+      <input type="radio" name="gender" value="F">Female
+      <input type="submit" value="Submit">
+    </form>  
+  </body>
+
+</html>
