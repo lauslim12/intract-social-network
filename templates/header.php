29430a85b477b3396f6185491118839ef96d9422
Nicholas <nicholasdwiarto@yahoo.com>
2020-03-02 23:09:30 +0700

    Modified template for the header.

--- templates/header.php
+++ templates/header.php
@@ -14,5 +14,31 @@
 <html>
   <head>
     <title>Intract</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" 
+        crossorigin="anonymous"></script>
+
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg navigation">
+      <div class="navigation__logo">
+        <a class="navigation__logo--link" href="index.php">Intract!</a>
+      </div>
+
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">Profile</li>
+        <li class="nav-item">Messages</li>
+        <li class="nav-item">Settings</li>
+      </ul>
+      
+    </nav>
