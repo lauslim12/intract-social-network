54e9e5b37516d267002d8444185e98c31563e407
Nicholas <nicholasdwiarto@yahoo.com>
2020-03-03 20:02:16 +0700

    Sample profile page for users.

---
+++ profile.php
@@ -0,0 +1,14 @@
+<?php
+  include 'templates/header.php';
+?>
+
+  <div class="container">
+    <div class="user-details">
+      <div class="user-details__wall">
+        <p>This is profile page.</p>
+      </div>    
+    </div>
+  </div>
+  
+  </body>
+</html>
