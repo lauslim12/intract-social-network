dcea5aa8a1aaf8e6cb4cf6b9b14f55f44570e248
Nicholas <nicholasdwiarto@yahoo.com>
2020-03-03 20:00:07 +0700

    Added table for posts.

--- dev/database.sql
+++ dev/database.sql
@@ -19,3 +19,33 @@ CREATE TABLE IF NOT EXISTS users (
   friend_array TEXT,
   PRIMARY KEY (id, username, email)
 );
+
+CREATE TABLE IF NOT EXISTS posts (
+  id INT NOT NULL AUTO_INCREMENT,
+  body TEXT,
+  added_by VARCHAR(60),
+  user_to VARCHAR(60),
+  date_added DATETIME,
+  user_closed VARCHAR(3),
+  deleted VARCHAR(3),
+  likes INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS post_comments (
+  id INT NOT NULL AUTO_INCREMENT,
+  post_body TEXT,
+  posted_by VARCHAR(60),
+  posted_to VARCHAR(60),
+  date_added DATETIME,
+  removed VARCHAR(3),
+  post_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS likes (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(60),
+  post_id INT,
+  PRIMARY KEY (id)
+);
