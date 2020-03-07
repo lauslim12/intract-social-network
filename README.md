f9a264716e47fc69dc211bf7db908bd707da5301
Nicholas <nicholasdwiarto@yahoo.com>
2020-03-07 19:00:54 +0700

    Added infinite scrolling and temporary job descriptions.

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Intract Social Networking Site
 Intract social networking site, built with PHP, MariaDB, JavaScript.
 
+## Catatan Khusus Kuliah
+Jobdesc:
+- Sign Up System -> Nicholas Dwiarto, Sebastian
+- Login System -> Nicholas Dwiarto, Sinzie Winata
+- Profile System (Ganti DP, Display Information) -> Nicholas Dwiarto, Attar Kusuma Pratiwa (Ganti DP OTW Progress)
+- Infinite Scrolling -> Nicholas Dwiarto
+- User Interface -> Nicholas Dwiarto
+
+Don't forget to remove those lines after the midterm assignment is graded.
+
 ## Architecture / Philosophies
 * Object Oriented.
 * IBM Programming Style and Techniques.
@@ -16,6 +26,7 @@ For now, the website has three features, more will be added as the project progr
 * Sign Up System
 * Login System
 * Profile System
+* Wall System (Complete with infinite scrolling!)
 
 TODO:
 * Sanitize input with `htmlspecialchars` and `filter_var()` after `strip_tags()` in register and login handler.
@@ -35,7 +46,7 @@ TODO:
 * Run all for production code.
 
 ## Project Structure
-The project structure will follow the Model, View, Controller style with a mixed addition of PHP Official Structure from https://docs.php.earth/faq/misc/structure/, and PHP Standards Recommendation (PSR) from http://www.php-fig.org/psr/.
+The project structure will follow the Model (Classes), View, Controller style with a mixed addition of PHP Official Structure from https://docs.php.earth/faq/misc/structure/, and PHP Standards Recommendation (PSR) from http://www.php-fig.org/psr/.
 
 ## Installations and Usage
 * Use PHP with version more than 5.5.
--- index.php
+++ index.php
@@ -28,13 +28,67 @@
 
       <br>
 
-      <?php
-        $post = new Post($db, $user_logged_in);
-        $post->load_posts_friends();
-      ?>
-
+      <div class="user-details__posts-area">
+        <!-- Placeholder div for placing the posts. -->
+      </div>
+      <img class="user-details--loading" src="assets/images/icons/loading.gif" alt="Loading Icon">
 
     </div>
+
+    <script>
+      let user_logged_in = '<?php echo $user_logged_in ?>';
+
+      $(document).ready(function() {
+        $('.user-details--loading').show();
+
+        // AJAX request for calling posts.
+        $.ajax({
+          url: 'src/controllers/handlers/ajax_php_load_posts.php',
+          type: 'POST',
+          data: 'page=1&user_logged_in=' + user_logged_in,
+          cache: false,
+
+          success: function(data) {
+            $('.user-details--loading').hide();
+            $('.user-details__posts-area').html(data);
+          }
+
+        });
+
+        $(window).scroll(function() {
+          // Container height containing the posts.
+          let height = $('.user-details__posts-area').height();
+          let scroll_top = $(this).scrollTop();
+          let page = $('.user-details__posts-area').find('.user-details__posts-area__next-post').val();
+          let final_post = $('.user-details__posts-area').find('.user-details__posts-area__final-post').val();
+
+          if(($(window).scrollTop() + $(window).height() > $(document).height() - 25) && final_post == 'false') {
+            $('.user-details--loading').show();
+
+            let ajax_request = $.ajax({
+              url: 'src/controllers/handlers/ajax_php_load_posts.php',
+              type: "POST",
+              data: "page=" + page + "&user_logged_in=" + user_logged_in,
+              cache: false,
+
+              success: function(response) {
+                $('.user-details__posts-area').find('.user-details__posts-area__next-post').remove();
+                $('.user-details__posts-area').find('.user-details__posts-area__final-post').remove();
+
+                $('.user-details--loading').hide();
+                $('.user-details__posts-area').append(response);
+              }
+
+            });
+          } // End if.
+          else {
+            return false;
+          }
+        }); // End of window scroll function.
+
+      });
+
+    </script>
 
   </div>
   
