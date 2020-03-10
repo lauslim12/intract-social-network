d8c0de036f7285e88059400a02a2a53dd72ae6c0
Nicholas <nicholasdwiarto@yahoo.com>
2020-03-10 20:32:19 +0700

    Modified the main page, and added more functions.

--- index.php
+++ index.php
@@ -8,36 +8,274 @@
     $post->submit_post($_POST['post_text'], 'none');
     header("location: index.php");
   }
-
 ?>
-  
-  <div class="user-details">
-    <div class="user-details__profile">
-      <img src=<?php echo $user['profile_pic'] ?> alt="Profile Picture">
-      <a href=<?php echo $user_logged_in ?>><?php echo $user['first_name'] . " " . $user['last_name'] ?></a>
-      <p><?php echo "Posts: " . $user['num_posts'] ?></p>
-      <p><?php echo "Likes: " . $user['num_likes'] ?></p>
-    </div>
 
-    <div class="user-details__wall">
-      <form action='' method="POST" class="user-details--form">
-        <textarea name="post_text" id="post_text" rows="5" cols="100" placeholder="A penny for your thoughts?"></textarea>
-        <input type="submit" value="Post" name="post" id="post_button">
-      </form>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="shortcut icon" type="image/png" href="assets/images/icons/favicon.png">
+        
+        <title>Intract x Bordeaux &mdash; Your Reviews</title>
+    </head>
+    <body>
+        <div class="container">
+            <header class="header">
+                <img src="assets/images/icons/logo.png" alt="Trillo logo" class="logo">
 
-      <br>
+                <form action="search.php" class="search" name="search_form">
+                    <input type="text" class="search__input" name="q" autocomplete="off" placeholder="Search other reviewers...">            
+                    <button class="search__button">
+                        <svg class="search__icon">
+                            <use xlink:href="assets/images/svg/sprite.svg#icon-magnifying-glass"></use>
+                        </svg>
+                    </button>
+                </form>
 
-      <div class="user-details__posts-area">
-        <!-- Placeholder div for placing the posts. -->
-      </div>
-      <img class="user-details--loading" src="assets/images/icons/loading.gif" alt="Loading Icon">
+                <nav class="user-nav">
+                    <div class="user-nav__icon-box">
+                        <svg class="user-nav__icon">
+                            <use xlink:href="assets/images/svg/sprite.svg#icon-bookmark"></use>
+                        </svg>
+                        <span class="user-nav__notification">5</span>
+                    </div>
 
-    </div>
+                    <div class="user-nav__icon-box">
+                        <svg class="user-nav__icon">
+                            <use xlink:href="assets/images/svg/sprite.svg#icon-chat"></use>
+                        </svg>
+                        <span class="user-nav__notification">10</span>
+                    </div>
 
-    <script src="assets/js/search.js"></script>
-    <script src="assets/js/ajax_js_load_posts.js"></script>
+                    <div class="user-nav__user">
+                        <img src="<?php echo $user['profile_pic']; ?>" alt="User photo" class="user-nav__user-photo">
+                        <span class="user-nav__user-name"><a href="<?php echo $user_logged_in; ?>"><?php echo $user_logged_in; ?></a></span>
+                    </div>
 
-  </div>
-  
-  </body>
+                    <div class="user-nav__user">
+                        <span class="user-nav__user-name"><a href="src/controllers/handlers/logout.php">Logout</a></span>
+                    </div>
+                </nav>
+            </header>
+
+            <div class="content">
+                <nav class="sidebar">
+                    <ul class="side-nav">
+                        <li class="side-nav__item side-nav__item--active">
+                            <a href="#" class="side-nav__link">
+                                <svg class="side-nav__icon">
+                                    <use xlink:href="assets/images/svg/sprite.svg#icon-home"></use> 
+                                </svg>
+                                <span>Hotel</span>
+                            </a>
+                        </li>
+
+                        <li class="side-nav__item side-nav__item">
+                            <a href="<?php echo $user_logged_in; ?>" class="side-nav__link">
+                                <svg class="side-nav__icon">
+                                    <use xlink:href="assets/images/svg/sprite.svg#icon-aircraft-take-off"></use> 
+                                </svg>
+                                <span>Profile</span>
+                            </a>
+                        </li>
+
+                    </ul>
+
+                    <div class="legal">
+                        &copy; 2020 by Intract and Bordeaux. All Rights Reserved.
+                    </div>
+                </nav>
+
+                <main class="hotel-view">
+                    <div class="gallery">
+                        <figure class="gallery__item">
+                            <img src="assets/images/photos/hotel-1.jpg" alt="Photo 1" class="gallery__photo">
+                        </figure>
+
+                        <figure class="gallery__item">
+                            <img src="assets/images/photos/hotel-2.jpg" alt="Photo 2" class="gallery__photo">
+                        </figure>
+
+                        <figure class="gallery__item">
+                            <img src="assets/images/photos/hotel-3.jpg" alt="Photo 3" class="gallery__photo">
+                        </figure>
+                    </div>
+
+                    <div class="overview">
+                        <h1 class="overview__heading">Bordeaux Le Grand Hôtel</h1>
+                        <div class="overview__stars">
+                            <svg class="overview__icon-star">
+                                <use xlink:href="assets/images/svg/sprite.svg#icon-star"></use>
+                            </svg>
+                            <svg class="overview__icon-star">
+                                <use xlink:href="assets/images/svg/sprite.svg#icon-star"></use>
+                            </svg>
+                            <svg class="overview__icon-star">
+                                <use xlink:href="assets/images/svg/sprite.svg#icon-star"></use>
+                            </svg>
+                            <svg class="overview__icon-star">
+                                <use xlink:href="assets/images/svg/sprite.svg#icon-star"></use>
+                            </svg>
+                            <svg class="overview__icon-star">
+                                <use xlink:href="assets/images/svg/sprite.svg#icon-star"></use>
+                            </svg>
+                        </div>
+
+                        <div class="overview__location">
+                            <svg class="overview__icon-location">
+                                <use xlink:href="assets/images/svg/sprite.svg#icon-location-pin"></use>
+                            </svg>
+                            <button class="btn-inline">Bordeaux, France</button>
+                        </div>
+                            
+                        <div class="overview__rating">
+                            <div class="overview__rating-average">4.5</div>
+                            <div class="overview__rating-count">888 votes</div>
+                        </div>
+                    </div>
+
+                    <div class="detail">
+                        <div class="description">
+                        
+                            <p class="paragraph">
+                                An unforgettable journey in the spirit of time in the beating heart of an historic heaven.
+                                From fine cuisine to the moments of relaxation offered by the Spa Guerlain, offer yourself some exceptional moments.
+                                Our services work in unison in order to provide you the best quality of reception all along your stay, 24/7.
+                            </p>
+                            <p class="paragraph">
+                                InterContinental Bordeaux – Le Grand Hotel is one of the best luxury hotels located in Bordeaux, the magnificent wine region of France. 
+                                Book now!
+                            </p>
+
+                            <ul class="list">
+                                <li class="list__item">Close to the beach</li>
+                                <li class="list__item">Breakfast included</li>
+                                <li class="list__item">Free airport shuttle</li>
+                                <li class="list__item">Free wifi in all rooms</li>
+                                <li class="list__item">Close to the museum of France!</li>
+                                <li class="list__item">Luxurious hotel</li>
+                                <li class="list__item">Comfortable bed</li>
+                                <li class="list__item">Ancient style, modern services</li>
+                            </ul>
+
+                            <div class="recommend">
+                                <p class="paragraph__count">
+                                    Nicholas and 5 other friends recommend this place!
+                                </p>
+                                <div class="recommend__friends">
+                                    <img src="assets/images/photos/user-3.jpg" alt="Friend 3" class="recommend__photo">
+                                    <img src="assets/images/photos/user-4.jpg" alt="Friend 4" class="recommend__photo">
+                                    <img src="assets/images/photos/user-5.jpg" alt="Friend 5" class="recommend__photo">
+                                    <img src="assets/images/photos/user-6.jpg" alt="Friend 6" class="recommend__photo">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="user-reviews">
+                            <figure class="review">
+                                <blockquote class="review__text">
+                                    During a trip to France this summer, I spent my last night at the InterContinental Bordeaux Le Grand Hôtel. 
+                                    I’d actually stayed there several years before when it was part of Regent Hotels, but was eager to stay again now that it’s part of IHG.
+                                </blockquote>   
+                                <figcaption class="review__user">
+                                    <img src="assets/images/photos/user-1.jpg" alt="Photo of reviewer 1" class="review__photo">
+                                    <div class="review__user-box">
+                                        <p class="review__user-name">Nicholas Dwiarto</p>
+                                        <p class="review__user-date">Dec 31st, 2019</p>
+                                    </div>
+                                    <div class="review__rating">4.5</div>
+                                </figcaption>
+                            </figure>
+
+                            <figure class="review">
+                                <blockquote class="review__text">
+                                    Our view overlooked one of the hotel’s central courtyards, which was nice and quiet, and the windows actually opened, so we could enjoy some fresh air.
+                                </blockquote>   
+                                <figcaption class="review__user">
+                                    <img src="assets/images/photos/user-2.jpg" alt="Photo of reviewer 2" class="review__photo">
+                                    <div class="review__user-box">
+                                        <p class="review__user-name">Marie Julis-Alexia</p>
+                                        <p class="review__user-date">Jan 1st, 2020</p>
+                                    </div>
+                                    <div class="review__rating">4.75</div>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+
+                    <div class="detail">
+                        <div class="description">
+                            <h2>REVIEWS</h2>
+                            <form action='' method="POST">
+                                <textarea name="post_text" id="post_text" rows="5" placeholder="A penny for your thoughts?"></textarea>
+                                <input type="submit" value="Post" name="post" id="post_button" class="btn-inline">
+                            </form>
+
+                            <div class="posts-area">
+                                <!-- Placeholder Div -->
+                            </div>
+
+                            <img class="posts-area--loading" src="assets/images/icons/loading.gif" alt="Loading Icon">
+                    
+                        </div>
+
+                        <div class="user-reviews">
+                            <figure class="review">
+                                <blockquote class="review__text">
+                                    During a trip to France this summer, I spent my last night at the InterContinental Bordeaux Le Grand Hôtel. 
+                                    I’d actually stayed there several years before when it was part of Regent Hotels, but was eager to stay again now that it’s part of IHG.
+                                </blockquote>   
+                                <figcaption class="review__user">
+                                    <img src="assets/images/photos/user-1.jpg" alt="Photo of reviewer 1" class="review__photo">
+                                    <div class="review__user-box">
+                                        <p class="review__user-name">Nicholas Dwiarto</p>
+                                        <p class="review__user-date">Dec 31st, 2019</p>
+                                    </div>
+                                    <div class="review__rating">4.5</div>
+                                </figcaption>
+                            </figure>
+
+                            <figure class="review">
+                                <blockquote class="review__text">
+                                    Our view overlooked one of the hotel’s central courtyards, which was nice and quiet, and the windows actually opened, so we could enjoy some fresh air.
+                                </blockquote>   
+                                <figcaption class="review__user">
+                                    <img src="assets/images/photos/user-2.jpg" alt="Photo of reviewer 2" class="review__photo">
+                                    <div class="review__user-box">
+                                        <p class="review__user-name">Marie Julis-Alexia</p>
+                                        <p class="review__user-date">Jan 1st, 2020</p>
+                                    </div>
+                                    <div class="review__rating">4.75</div>
+                                </figcaption>
+                            </figure>
+                            <button class="btn-inline">Show all <span>&rarr;</span></button>
+                        </div>
+                    </div>
+
+                    <div class="cta">
+                        <h2 class="cta__book-now">
+                            Good news! We have 2 free rooms for your selected dates!
+                        </h2>
+                        <button class="btn">
+                            <span class="btn__visible">Book now!</span>
+                            <span class="btn__invisible">Only 2 rooms left!</span>
+                        </button>
+                    </div>
+                </main>
+
+            </div>
+        </div>
+
+        <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous"></script>
+        <script src="assets/js/search.js"></script>
+        <script src="assets/js/ajax_js_load_posts.js"></script>
+
+    </body>
 </html>
