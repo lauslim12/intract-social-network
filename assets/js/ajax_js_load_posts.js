63f0e4fd9030894e7b17694e7a292dfd184dd28c
Nicholas <nicholasdwiarto@yahoo.com>
2020-03-10 20:24:20 +0700

    Modified AJAX call and search query.

--- assets/js/ajax_js_load_posts.js
+++ assets/js/ajax_js_load_posts.js
@@ -2,7 +2,7 @@
 let user_logged_in = '<?php echo $user_logged_in ?>';
 
 $(document).ready(function() {
-  $('.user-details--loading').show();
+  $('.posts-area--loading').show();
 
   // AJAX request for calling posts.
   $.ajax({
@@ -12,26 +12,26 @@ $(document).ready(function() {
     cache: false,
 
     success: function(data) {
-      $('.user-details--loading').hide();
-      $('.user-details__posts-area').html(data);
+      $('.posts-area--loading').hide();
+      $('.posts-area').html(data);
     }
 
   });
 
   $(window).scroll(function() {
     // Container height containing the posts.
-    let height = $('.user-details__posts-area').height();
-    let scroll_top = $(this).scrollTop();
-    let page = $('.user-details__posts-area').find('.user-details__posts-area__next-post').val();
-    let final_post = $('.user-details__posts-area').find('.user-details__posts-area__final-post').val();
+    let height = window.pageYOffset;
+    let offset = $('.posts-area').offset().top;
+    let page = $('.posts-area').find('.posts-area__next-post').val();
+    let final_post = $('.posts-area').find('.posts-area__final-post').val();
     
     // We have to prevent the race condition, because scrollTop function can occur more than once!
     // This is called lock variable.
     let loading = false;
 
-    if(($(window).scrollTop() + $(window).height() > $(document).height() - 25) && final_post == 'false' && loading == false) {
+    async function load_more() {
       loading = true;
-      $('.user-details--loading').show();
+      $('.posts-area--loading').show();
 
       let ajax_request = $.ajax({
         url: 'src/controllers/handlers/ajax_php_load_posts.php',
@@ -40,15 +40,22 @@ $(document).ready(function() {
         cache: false,
 
         success: function(response) {
-          $('.user-details__posts-area').find('.user-details__posts-area__next-post').remove();
-          $('.user-details__posts-area').find('.user-details__posts-area__final-post').remove();
+          $('.posts-area').find('.posts-area__next-post').remove();
+          $('.posts-area').find('.posts-area__final-post').remove();
 
-          $('.user-details--loading').hide();
-          $('.user-details__posts-area').append(response);
+          $('.posts-area--loading').hide();
+          $('.posts-area').append(response);
+          $('.posts-area').removeClass("loaded");
           loading = false;
         }
       });
+    }
 
+    if((height > offset) && final_post == 'false' && loading == false) {
+      if(!$('.posts-area').hasClass("loaded")) {
+        $('.posts-area').addClass("loaded");
+        load_more();
+      }
     } // End if.
     else {
       return false;
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-  // Lengthen the search bar.
-  $('#search_text_input').focus(function() {
-    if(window.matchMedia("(min-width: 800px)").matches) {
-      $(this).animate({width: '25rem'}, 500);
-    }
-  });
-
-  $('.search__button-holder').click(function() {
+  $('.search__button').click(function() {
     $('.search_form').submit();
   });
 
