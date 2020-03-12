1bcb314804e603302e689e0af62a9cf816d473f9
Nicholas <nicholasdwiarto@yahoo.com>
2020-03-12 16:56:56 +0700

    Added templating for the pages.

---
+++ templates/sidebar.php
@@ -0,0 +1,26 @@
+<nav class="sidebar">
+  <ul class="side-nav">
+    <li class="side-nav__item side-nav__item--active">
+      <a href="#" class="side-nav__link">
+        <svg class="side-nav__icon">
+          <use xlink:href="assets/images/svg/sprite.svg#icon-home"></use>
+        </svg>
+        <span>Hotel</span>
+      </a>
+    </li>
+
+    <li class="side-nav__item side-nav__item">
+      <a href="<?php echo $user_logged_in; ?>" class="side-nav__link">
+        <svg class="side-nav__icon">
+          <use xlink:href="assets/images/svg/sprite.svg#icon-aircraft-take-off"></use>
+        </svg>
+        <span>Profile</span>
+      </a>
+    </li>
+
+  </ul>
+
+  <div class="legal">
+    &copy; 2020 by Intract and Bordeaux. All Rights Reserved.
+  </div>
+</nav>
